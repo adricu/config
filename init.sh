681cfedcee9b12b6cdc075b5454382ba26d315f6
Adrián Cuesta <adricu@adrians-mbp.lan>
2018-12-24 02:30:26 +0100

    Install and setup nerd-fonts

--- init.sh
+++ init.sh
@@ -1,10 +1,22 @@
 # Brew Packages
-brew install thefuck pyenv pipenv yarn jq node go kubernetes-cli kubernetes-helm heroku
+brew install thefuck yarn jq node kubernetes-cli kubernetes-helm
+
+# Install python tools
+brew install pyenv pipenv
+export LDFLAGS="-L/usr/local/opt/zlib/lib"
+export CPPFLAGS="-I/usr/local/opt/zlib/include"
+pyenv install 3.6.7
 
 # ZSH & others
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-## Powerlevel9k (needs Powerline Font: https://github.com/bhilburn/powerlevel9k/wiki/Install-Instructions#step-2-install-a-powerline-font)
+
+## Powerlevel9k & "Hack Nerd Font"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+brew tap caskroom/fonts
+brew cask install font-hack-nerd-font
+
 ## zsh-syntax-highlighting plugin
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Link .zshrc
 ln -s ~/.adricu/dotfiles/zshrc .zshrc
